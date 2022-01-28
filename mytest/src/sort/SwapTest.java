7ee78a9f16ce2b047b54c6fe9b969c4f1ce22a85
Suuukim <toki159@naver.com>
2022-01-28 15:35:18 +0900

    intSortTest

---
+++ mytest/src/sort/SwapTest.java
@@ -0,0 +1,17 @@
+package sort;
+
+public class SwapTest {
+	public static void main(String[] args) {
+		
+		int a = 11, b = 22;
+		int temp;
+		
+		temp = b;
+		b = a ;
+		a=temp;
+		
+		System.out.println("a= " + a + "b= " + b );
+	
+	
+	}
+}
