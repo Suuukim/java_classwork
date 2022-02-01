7e8d268da1a113e6c8b644004f2e592be90eab55
jeongsu kim <toki159@naver.com>
2022-02-01 23:02:23 +0900

    배열

---
+++ java.day01 to 16/src/day04/ArrayEx1.java
@@ -0,0 +1,34 @@
+package day04;
+
+import java.util.Arrays;
+
+public class ArrayEx1 {
+	public static void main(String[] args) {
+		int[] score = new int[5];
+		int k = 1;
+		
+		score[0] = 50;
+		score[1] = 60;
+		score[k+1] = 70;
+		score[2] = 80;
+		score[3] = 90;
+		
+		System.out.println("score : " + Arrays.toString(score));
+		
+		int tmp = score[k+2] + score[4];
+		System.out.println("tmp : " + tmp);
+		System.out.println();
+		
+		for(int i=0;i<score.length;i++) {
+			System.out.println("score [" + i + "] : " + score[i]);
+		}
+		
+		for(int ele : score) {
+			System.out.println("ele : " + ele);
+		}
+		
+		System.out.println(score[5]); //에러
+		//길이 vs 인덱스
+		
+	}
+}
