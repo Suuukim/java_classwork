183136eebb9228aae6caec8bb4ad4286feb2b0da
jeongsu kim <toki159@naver.com>
2023-02-09 16:32:23 +0900

    데이터 타입

---
+++ javatutorials/src/Variable/IntDatatypeDemo.java
@@ -0,0 +1,14 @@
+package Variable;
+
+public class IntDatatypeDemo {
+	public static void main(String[] args) {
+		byte a;
+//		a = 128;		//byte의 표현가능 범위 : -128 ~ 127
+		
+		short b;
+//		b = 32768;		//short의 표현가능 범위 : -32,768 ~ 32,767
+		
+		
+		
+	}
+}
